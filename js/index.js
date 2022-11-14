3666abda31cf05e651d350f94f3b53a0a6232da6
SErgioNbP <sergionevespinheiro@gmail.com>
2022-11-14 00:16:27 +0000

    maze creation continued

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 
-const cells = 3;
+const cells = 10;
+const wallThickness = 10;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
     element: document.body,
@@ -21,10 +25,10 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
 World.add(world, walls);
 
@@ -87,7 +91,7 @@ const stepThroughCell = (row, column) => {
             continue;
         }
         
-        // If we visited neighbor, continue to next neighbor
+        // If neighbor visited, continue to next neighbor
         if (grid[nextRow][nextColumn]){
             continue;
         }
@@ -102,9 +106,85 @@ const stepThroughCell = (row, column) => {
         } else if (direction === 'down') {
             horizontals[row][column] = true;
         }
+        
+        // Visit next cell
+        stepThroughCell(nextRow, nextColumn);
     }
-
-    // Visit next cell
 }
 
 stepThroughCell(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if(open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength / 2,
+            rowIndex * unitLength + unitLength,
+            unitLength,
+            wallThickness,
+            {
+                isStatic: true
+            }
+        );
+        World.add(world, wall);
+    });
+});
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if(open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            wallThickness,
+            unitLength,
+            {
+                isStatic: true
+            }
+        );
+        World.add(world, wall);
+    });
+});
+
+// Goal
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * .7,
+    unitLength * .7,
+    {
+        isStatic: true
+    }
+);
+World.add(world, goal);
+
+// Ball
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength / 3
+);
+World.add(world, ball);
+
+document.addEventListener('keydown', event => {
+    const { x, y } = ball.velocity;
+
+    if (event.key === 'w') {
+        Body.setVelocity(ball, { x, y: y - 5 });
+    }
+    if (event.key === 'd') {
+        Body.setVelocity(ball, { x: x + 5, y });
+    }
+    if (event.key === 's') {
+        Body.setVelocity(ball, { x, y: y + 5 });
+    }
+    if (event.key === 'a') {
+        Body.setVelocity(ball, { x: x - 5, y });
+    }
+});
